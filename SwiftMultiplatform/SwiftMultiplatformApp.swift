ec3594562b4447ad7ba52783c6bbc5be7551fcb5
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-06-11 17:47:20 +0500

    Initial Commit

---
+++ SwiftMultiplatform/SwiftMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMultiplatformApp.swift
+//  SwiftMultiplatform
+//
+//  Created by Shayan Ameen on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
