9a8d508fca34eb1dd420c7d7d4f521bc1406f5d6
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-06-14 09:25:20 +0500

    random colors on tap

--- SwiftMultiplatform/TextView.swift
+++ SwiftMultiplatform/TextView.swift
@@ -9,17 +9,35 @@ import SwiftUI
 
 struct TextView: View {
     let text: String
-    let color: Color
-    var opacity: Double = 0.45
+    @State
+    var color: Color
+    
+    let colors: [Color] = [
+        .red,
+        .green,
+        .blue,
+        .orange,
+        .yellow,
+        .purple,
+        Color(red: 0.5, green: 0, blue: 0.5),
+        Color(red: 0, green: 0.5, blue: 0.5),
+        Color(red: 139/255, green: 207/255, blue: 240/255),
+        Color(red: 1, green: 215/255, blue: 0.5),
+    ]
     
     var body: some View {
         Text(text)
             .fontWeight(.semibold)
             .padding()
             .foregroundStyle(.white)
-            .background(color.opacity(opacity))
+            .background(color.opacity(0.45))
             .cornerRadius(20)
             .shadow(color: color, radius: 5, x: 5, y: 5)
+            .onTapGesture {
+                withAnimation {
+                    color = colors.randomElement() ?? .red     
+                }
+            }
     }
 }
 
